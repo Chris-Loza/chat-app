69d78d78ded989eb93c754cf002d74d770388699
Chris-Loza <christianloza99@gmail.com>
2024-09-13 14:39:15 -0700

    added UserInfo component structure and styling

--- src/components/list/userInfo/UserInfo.jsx
+++ src/components/list/userInfo/UserInfo.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./userInfo.css";
 
 const UserInfo = () => {
-  return <div className="userInfo">UserInfo</div>;
+  return <div className="userInfo">
+    <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>John Doe</h2>
+    </div>
+    <div className="icons">
+        <img src="./more.png" alt="more icon" />
+        <img src="./video.png" alt="video icon" />
+        <img src="./edit.png" alt="edit icon" />
+    </div>
+  </div>;
 };
 
 export default UserInfo;
