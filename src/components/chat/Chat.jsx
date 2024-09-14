8328a16a3f990a0ca02df17cd16ad0d4a01b033d
Chris-Loza <christianloza99@gmail.com>
2024-09-13 17:17:05 -0700

    Chat component content and styling added

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,8 +1,100 @@
-import React from "react";
+import { useState } from "react";
 import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
+
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+    setOpen(false);
+  };
+
+  return (
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="avatar" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>User description</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="phone icon" />
+          <img src="./video.png" alt="video icon" />
+          <img src="./info.png" alt="info icon" />
+        </div>
+      </div>
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="avatar" />
+          <div className="texts">
+            <p>Message text</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>Message text</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="avatar" />
+          <div className="texts">
+            <p>Message text</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>Message text</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="avatar" />
+          <div className="texts">
+            <p>Message text</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img src="./ChatAppBG.webp" alt="test image" />
+            <p>Message text</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="image icon" />
+          <img src="./camera.png" alt="camera icon" />
+          <img src="./mic.png" alt="microphone icon" />
+        </div>
+        <input
+          type="text"
+          placeholder="Type a message..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="emoji">
+          <img
+            src="./emoji.png"
+            alt="emoji icon"
+            onClick={() => setOpen(!open)}
+          />
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
