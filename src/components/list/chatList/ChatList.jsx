3fd4a8b7daf3807f5aacdcce85662cfef8893d16
Chris-Loza <christianloza99@gmail.com>
2024-09-20 15:54:20 -0700

    implemented real time chatlist data gathering in ChatList component

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,9 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./chatList.css";
 import AddUser from "./addUser/AddUser";
+import { useUserStore } from "../../../lib/userStore.js";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { db } from "../../../lib/firebase.js";
 
 const ChatList = () => {
   const [addMode, setAddMode] = useState(false);
+  const [chats, setChats] = useState([]);
+  const { currentUser } = useUserStore();
+
+  useEffect(() => {
+    const unSub = onSnapshot(
+      doc(db, "userchats", currentUser.id),
+      async (res) => {
+        const items = res.data().chats;
+
+        const promises = items.map(async (item) => {
+          const userDocRef = doc(db, "users", item.receiverId);
+          const userDocSnap = await getDoc(userDocRef);
+
+          const user = userDocSnap.data();
+
+          return { ...item, user };
+        });
+
+        const chatData = await Promise.all(promises);
+
+        setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
+      }
+    );
+
+    return () => {
+      unSub();
+    };
+  }, [currentUser.id]);
 
   return (
     <div className="chatList">
@@ -20,21 +51,16 @@ const ChatList = () => {
         />
       </div>
 
-      <div className="item">
-        <img src="./avatar.png" alt="avatar" />
-        <div className="texts">
-          <span>Jane Doe</span>
-          <p>Hello</p>
+      {chats.map((chat) => (
+        <div className="item" key={chat.chatId}>
+          <img src="./avatar.png" alt="avatar" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>{chat.lastMessage}</p>
+          </div>
         </div>
-      </div>
+      ))}
 
-      <div className="item">
-        <img src="./avatar.png" alt="avatar" />
-        <div className="texts">
-          <span>Jane Doe</span>
-          <p>Hello</p>
-        </div>
-      </div>
       {addMode && <AddUser />}
     </div>
   );
