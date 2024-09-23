2cea11eb5ee0476f0456ce01f3a51233175e9f69
Chris-Loza <christianloza99@gmail.com>
2024-09-23 16:41:38 -0700

    added search chats functionality

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -9,6 +9,8 @@ import { useChatStore } from "../../../lib/chatStore.js";
 const ChatList = () => {
   const [addMode, setAddMode] = useState(false);
   const [chats, setChats] = useState([]);
+  const [input, setInput] = useState("");
+
   const { currentUser } = useUserStore();
   const { chatId, changeChat } = useChatStore();
 
@@ -62,12 +64,20 @@ const ChatList = () => {
     }
   };
 
+  const filteredChats = chats.filter((c) =>
+    c.user.username.toLowerCase().includes(input.toLowerCase())
+  );
+
   return (
     <div className="chatList">
       <div className="search">
         <div className="searchBar">
           <img src="./search.png" alt="search icon" />
-          <input type="text" placeholder="Search" />
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={(e) => setInput(e.target.value)}
+          />
         </div>
         <img
           src={addMode ? "./minus.png" : "./plus.png"}
@@ -77,7 +87,7 @@ const ChatList = () => {
         />
       </div>
 
-      {chats.map((chat) => (
+      {filteredChats.map((chat) => (
         <div
           className="item"
           key={chat.chatId}
@@ -96,7 +106,7 @@ const ChatList = () => {
             <span>
               {chat.user.blocked.includes(currentUser.id)
                 ? "User"
-                : chat.username}
+                : chat.user.username}
             </span>
             <p>{chat.lastMessage}</p>
           </div>
